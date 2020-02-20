ec1b9367e9454e2a6fa9d795015ac891df107e48
fforius <60130637+fforius@users.noreply.github.com>
2020-02-20 17:32:37 +0700

    Update fakeload.py

--- userbot/modules/fun/fakeload.py
+++ userbot/modules/fun/fakeload.py
@@ -316,7 +316,7 @@ async def typewriter(typew):
 	sleep(0.03)
 	await typew.edit(str(number) + "%   ████████████████▌")
 	sleep(1)
-	await typew.edit("`Done!`")
+	await typew.edit("`Halahh Tertypu kamu bang sat!`")
 	# I did it for two hours :D just ctrl+c - crtl+v
 
 
